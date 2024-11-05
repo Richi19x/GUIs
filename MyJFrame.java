07dee60f14199237900e35ab5cdfca07fe4ee85f
Richi19x <cantorick99@gmail.com>
2024-11-05 08:50:18 -0600

    Add files via upload

--- MyJFrame.java
+++ MyJFrame.java
@@ -1,34 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.my.company.jframe;
 
-/**
- *
- * @author daniela
- */
+
+package com.mycompany.proyecto5nov;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class MyJFrame extends JFrame {
     private JLabel myLabel;
-
+    
     public MyJFrame() {
-        // Configuración de la ventana
-        setTitle("Mi Ventana");
-        setSize(250, 150);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        // Configuración de la etiqueta
-        myLabel = new JLabel("   Hola Mundo");
+        myLabel = new JLabel ();
+        myLabel.setText("Hola Mundo");
+        
         add(myLabel);
-        
-        // Mostrar la ventana
-        setVisible(true);
+        setTitle("MyJFrame");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
-        new MyJFrame();
+        MyJFrame frame = new MyJFrame();
+        frame.setVisible(true);
     }
 }
